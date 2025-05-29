3df09ec3a676541a83927464eee678838ef50664
Jacuzzi <jheklayag@gmail.com>
2025-05-29 22:48:02 +0800

    Include the quiz creator into a new class

---
+++ quiz_creator.py
@@ -0,0 +1,16 @@
+from questions import Question
+from validator import validate_correct_answer
+
+class QuizCreator:
+    def __init__(self):
+        self.questions = []
+        self.question_number = 1
+
+    def get_question_input(self):
+        pass
+
+    def save_questions_to_file(self, filename="questions_for_quiz.txt"):
+        pass
+
+    def run(self):
+        pass
